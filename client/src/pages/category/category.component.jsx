9ee588c86bba9dac5b08fae851a98e4deb0d914a
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-08-10 18:47:14 +0530

    Add basic category page

---
+++ client/src/pages/category/category.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './category.styles.scss';
+
+const CategoryPage  = () => (
+    <div className='category'>
+        <h1>Category Page</h1>
+    </div>
+);
+
+export default CategoryPage;
