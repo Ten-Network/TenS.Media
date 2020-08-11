a03a4fcc0347387091dcc0208d132caa5f0bd00d
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-08-11 17:54:51 +0530

    Correct the naming of collection page

---
+++ client/src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './collection.styles.scss';
+
+const CollectionPage  = ({ match }) => (
+    <div className='collection-page'>
+        <h1>Collection Page</h1>
+    </div>
+);
+
+export default CollectionPage;
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -3,12 +3,12 @@ import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collection/collection.component';
  
 const ShopPage = ({ match }) => ( 
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
