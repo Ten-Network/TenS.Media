a42b0a93b4abfcb41ac618a5571090695c81868b
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-08-12 18:10:48 +0530

    Integrate stripe into the website for payment processing

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publichableKey = 'pk_test_51Gt92lAHloLKFKhU77Od2GT8o7revPfpV6Yt6sW2XZjLHhYwjwFYRISG1boC6QuiaXJIe01RiS8kIFpMVijVQ8zy00fH3ZqkEB';
+
+    const onToken = token => (
+        alert('Payment Successfull')
+    )
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='Big Basket'
+            currency='INR'
+            description={`Your Total is $${price}`}
+            amount={priceForStripe}
+            token={onToken}
+            stripeKey={publichableKey}
+        />
+    );
+}
+
+export default StripeCheckoutButton;
