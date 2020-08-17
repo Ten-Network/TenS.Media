a1de6b9234ecdb710ef06ace01e3b7e04d9e3889
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-08-17 20:11:26 +0530

    Remove redux thunk library

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
-
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer);
 
 export const persistor = persistStore(store);
