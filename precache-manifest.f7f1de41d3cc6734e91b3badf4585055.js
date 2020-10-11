63229c1f301662a2020f36eda1ee43a2e2130077
unknown <malhotra.jatin1.jm@gmail.com>
2020-10-12 00:06:57 +0530

    Updates

---
+++ precache-manifest.f7f1de41d3cc6734e91b3badf4585055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c6f16cefd838707cf21133553b5b26",
+    "url": "/TenS.Media/index.html"
+  },
+  {
+    "revision": "d011e587819b87bed63e",
+    "url": "/TenS.Media/static/css/main.1ee5228a.chunk.css"
+  },
+  {
+    "revision": "334edc753a90dec7f567",
+    "url": "/TenS.Media/static/js/2.772e0023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TenS.Media/static/js/2.772e0023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d011e587819b87bed63e",
+    "url": "/TenS.Media/static/js/main.2c829f1d.chunk.js"
+  },
+  {
+    "revision": "e4997b9eeeeb48377f0e",
+    "url": "/TenS.Media/static/js/runtime-main.ed4af65a.js"
+  }
+]);
